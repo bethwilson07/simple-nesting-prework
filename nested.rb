24c5db80eb96072a013c3ab7c885454e7745e070
Beth Wilson <beth.ny.wlsn@gmail.com>
2018-09-22 20:36:49 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -64,11 +64,6 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP, C"]
-# }
 
 	programmer_hash = 
  		{
@@ -85,7 +80,16 @@ def adding_matz
           :languages => ["C"]
         }
      }
+# add the following information to the top level of programmer_hash
+# :yukihiro_matsumoto => {
+#   :known_for => "Ruby",
+#    :languages => ["LISP, C"]
+# }
 
+  programmer_hash[:yukihiro_matsumoto] = {
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+  }
     
 end
 
